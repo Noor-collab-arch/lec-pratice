44b910b8407e002c6f023596b13d3d2e5b557622
Noor-collab-arch <hnoor282@gmail.com>
2024-08-25 00:50:17 +0500

    added

--- Assignment.py
+++ Assignment.py
@@ -1,15 +1,37 @@
 #BMI
-user_height = input("enter your height in meter:")
+'''user_height = input("enter your height in meter:")
 user_weight = input("enter your weight in kilograms:")
 BMI = float(user_weight)/(float(user_height)**2)
 print("your bmi is " , BMI)
 
-#volume Volume=π×r^2 ×h
+#volume Volume=π×r^2 ×h'''
 
 
-height_cylinder:float = input("enter height of the cylinder:")
+''''height_cylinder:float = input("enter height of the cylinder:")
 radius_cylinder:float = input("enter radius of cylinder:")
 pi: float = 3.14159
 
 V = float(pi) * float(radius_cylinder)**2 * float(height_cylinder)
-print("volume of cylinder is" , V)
+print("volume of cylinder is" , V)'''
+
+#age calculate
+'''current_year : int = input("Enter current year:")
+birth_year: int = input("Enteryour birth year")
+
+age : int = int(current_year) - int(birth_year)
+print("Your age is :"  , age)'''
+
+
+
+# Area of a Rectangle: Formula: Area = length × width
+'''len : float = float(input("Enter the lenght"))
+wid: float = float(input("Enter width"))
+area_rect = float(len) * float(wid)
+print("The area of rectangle is:" , area_rect)'''
+
+
+#Area of a Circle: Formula: Area = π × radius²
+rad_cir = float(input("Enter the radius of circle:"))
+Pi = 3.14 
+area_circ = Pi*float(rad_cir) **2
+print("The area of circle is: ", area_circ)
